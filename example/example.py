01e6bc7e1b39de933fd21b4e95098dceae22a792
georgezywang <georgezywang@outlook.com>
2021-03-04 14:04:53 -0500

    refactor hiearchy

--- example/example.py
+++ example/example.py
@@ -3,6 +3,9 @@ from __future__ import print_function, division
 
 import numpy as np
 
+# import sys
+# sys.path.append("/Users/wzy/Desktop/George/COMPSCI/474/misvm/misvm")
+
 from misvmio import parse_c45, bag_set
 import misvm
 
